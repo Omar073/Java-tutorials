6a57c502e2c55895a2fd41867d7c399cf2797b29
Omar73 <120687551+Omar073@users.noreply.github.com>
2023-07-11 11:58:33 +0300

    50- JavaFX: drag a word with your mouse

---
+++ JavaFX/JavaFX example 12/App.java
@@ -0,0 +1,32 @@
+// 50- JavaFX: drag a word with your mouse
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    // Create a pane and set its properties
+    Pane pane = new Pane();
+    Text text = new Text(20, 20, "Programming is fun");
+    pane.getChildren().addAll(text);
+
+    text.setOnMouseDragged(e -> {
+        text.setX(e.getX());
+        text.setY(e.getY());
+    });
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 550, 400);
+    primaryStage.setTitle("MouseEventDemo"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+}
